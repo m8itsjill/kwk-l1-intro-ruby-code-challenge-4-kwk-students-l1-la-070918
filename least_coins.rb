7e61a0cfe9c5bd171d775590b0a8d88365ab4e81
m8itsjill <jillyusi@icloud.com>
2018-07-11 22:59:49 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,12 @@
 least_coins = {"quarters" => 0.25, "dimes" => 0.10, "nickels" => 0.5, "pennies" => 0.1} 
 
 def least_coins(cents)
-
+  coin_types = {
+    "quarters" => 0.25, 
+    "dimes" => 0.10, 
+    "nickels" => 0.5, 
+    "pennies" => 0.1
+    } 
 
 
 end
