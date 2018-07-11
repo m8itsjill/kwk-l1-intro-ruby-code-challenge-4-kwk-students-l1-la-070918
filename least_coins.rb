ddbbfc40533efe61eddbdb4b54f17d84ef09f715
m8itsjill <jillyusi@icloud.com>
2018-07-11 23:23:54 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -16,26 +16,26 @@ end
   end
 
   :dimes = cents/0.10
-  if dimes == 0.10
+  if :dimes == 0.10
     coin_types[:dimes] = 0.10
-  elsif dimes != 0.10
+  elsif :dimes != 0.10
     coin_types[:dimes] = dimes
-    cents = cents - dimes*0.10
+    cents = cents - :dimes*0.10
   end
 
-  nickels = cents/5
-  if nickels == 0.5
+  :nickels = cents/5
+  if :nickels == 0.5
     coin_types[:nickels] = 0.5
-  elsif nickels != 0.5
-    coin_types[:nickels] = nickels
-    cents = cents - nickels*0.5
+  elsif :nickels != 0.5
+    coin_types[:nickels] = :nickels
+    cents = cents - :nickels*0.5
   end
 
-  pennies = cents
-  if pennies == 0.1
+  :pennies = cents
+  if :pennies == 0.1
     coin_types[:pennies] = 0.1
   elsif pennies != 0.1
-    coin_types[:pennies] = pennies*0.1
+    coin_types[:pennies] = :pennies*0.1
   end
 
   puts coin_types
